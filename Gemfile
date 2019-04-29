e84a9b34a27ff4bdf505cf196a5374185961eec2
Alez <linzoom37@gmail.com>
2019-04-29 15:11:13 +0300

    Add rails_admin

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
-
+gem 'rails_admin'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_182824) do
+ActiveRecord::Schema.define(version: 2019_04_29_113925) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_03_24_182824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
